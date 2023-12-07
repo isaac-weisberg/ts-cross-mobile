96e4fa77c638edd5e112eb6cbe148fecd599e847
Isaac Weisberg <net.caroline.weisberg@gmail.com>
2023-12-08 00:46:48 +0500

    Add Kotlin code generation

--- Makefile
+++ Makefile
@@ -3,4 +3,5 @@ def: sample_proj
 sample_proj:
 	npx ts-node ./src/index.ts \
 		./sample_project/index.ts \
-		./build/SampleProjectGenerated.swift
+		./build/SampleProjectGenerated.swift \
+		./build/SampleProjectGenerated.kt
--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,20 @@ import * as ts from 'typescript'
 import * as fs from 'fs'
 import { findAllSymbols } from './Symbols/AllSymbols'
 import { generateSwiftFileForAllSymbols } from './Generators/SwiftGenerator'
+import { generateKotlinFileForAllSymbols } from './Generators/KotlinGenerator'
 
 const entryPointParam = process.argv[2]
 const swiftFileOutput = process.argv[3]
+const kotlinFileOutput = process.argv[4]
 
 if (!entryPointParam) {
     throw new Error('No entry point for the module to traverse')
 }
 if (!swiftFileOutput) {
     throw new Error('No Swift output file dir')
+}
+if (!kotlinFileOutput) {
+    throw new Error('No Kotlin output file dir')
 }
 
 if (!fs.existsSync(entryPointParam)) {
@@ -28,11 +33,17 @@ const program = ts.createProgram({
 
 const allSymbols = findAllSymbols(program)
 
+console.dir(allSymbols, { depth: null })
+
 const swiftGeneratedFile = generateSwiftFileForAllSymbols(allSymbols)
 
-console.dir(allSymbols, { depth: null })
 console.log('Swift generated file:')
 console.log(swiftGeneratedFile)
 
+const kotlinGeneratedFile = generateKotlinFileForAllSymbols(allSymbols)
+
+console.log('Kotlin generated file:')
+console.log(kotlinGeneratedFile)
 
 fs.writeFileSync(swiftFileOutput, swiftGeneratedFile)
+fs.writeFileSync(kotlinFileOutput, kotlinGeneratedFile)
